93d4640f72b0416b0ddcf6c9b5ec29af06e393d5
Chris Su <chrissuusc@gmail.com>
2015-12-30 12:08:59 -0800

    JdbcTemplate Added
    1.Remove Junit class Parameter.java
    2.Add JdbcTemplate into mysql and mssql Dao class
    3.Alter corresponding bean.xml file

--- src/com/five9/service/LsMSsqlConn.java
+++ src/com/five9/service/LsMSsqlConn.java
@@ -1,26 +1,27 @@
 package com.five9.service;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Service;
 
 import com.five9.model.Conn;
-import com.five9.model.Parameter;
 /*
  * <p> connection with Microsoft SQL Server (logisense report)</p>
  * 
  * */
 @Service("lsmssql")
 public class LsMSsqlConn  implements Conn{
-	private Parameter mssqlPara;
-
-	public Parameter getMssqlPara() {
+	private DataSource mssqlPara;
+	private JdbcTemplate jdbctemplate;
+	public DataSource getMssqlPara() {
 		return mssqlPara;
 	}
 
 	@Autowired//parameter name must meet with xml id name
-	public void setMssqlPara(Parameter mssqlPara) {
+	public void setMssqlPara(DataSource mssqlPara) {
 		this.mssqlPara = mssqlPara;
+		this.jdbctemplate= new JdbcTemplate(mssqlPara);
 	}
 
 	/* <p>This function is mainly for monitering purpose</p> 
@@ -31,11 +32,8 @@ public class LsMSsqlConn  implements Conn{
 	public void moniter(){
 		System.out.println();
 		System.out.println("Echo from logisense mssql server");
-		System.out.println(mssqlPara.getDriver());
-		System.out.println(mssqlPara.getPassword());
-		System.out.println(mssqlPara.getUsername());
-		System.out.println(mssqlPara.getUrl());
-		System.out.println(mssqlPara.getSql());
+//		System.out.println(this.jdbctemplate.getDriver());
+		System.out.println(mssqlPara.toString() );
 	}
 		
 }
--- src/com/five9/service/VccMysqlConn.java
+++ src/com/five9/service/VccMysqlConn.java
@@ -1,32 +1,30 @@
 package com.five9.service;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Service;
 
 import com.five9.model.Conn;
-import com.five9.model.Parameter;
 @Service("vccmysql")
 @Primary
 public class VccMysqlConn implements Conn{
-	private Parameter mysqlPara;
-	public Parameter getMysqlPara() {
+	private DataSource mysqlPara;
+	private JdbcTemplate jdbctemplate;
+	public DataSource getMysqlPara() {
 		return mysqlPara;
 	}
 	@Autowired//parameter name must meet with xml id name
-	public void setMysqlPara(Parameter mysqlPara) {
+	public void setMysqlPara(DataSource mysqlPara) {
 		this.mysqlPara = mysqlPara;
+		this.jdbctemplate = new JdbcTemplate(mysqlPara);
 	}
 	@Override
 	public void moniter(){
 		System.out.println();
 		System.out.println("Echo from vcc mysql server");
-		System.out.println(mysqlPara.getDriver());
-		System.out.println(mysqlPara.getPassword());
-		System.out.println(mysqlPara.getUsername());
-		System.out.println(mysqlPara.getUrl());
-		System.out.println(mysqlPara.getSql());
+		System.out.println(mysqlPara.toString());
 	}
 
 }
